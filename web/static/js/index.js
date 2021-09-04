1b773522f6ac0457615c29438fae6425f24dae33
Evgenij Ivankin <e.ivankin@innopolis.university>
2021-09-04 22:52:47 +0300

    metamask integration WIP

--- web/static/js/index.js
+++ web/static/js/index.js
@@ -1,21 +1,39 @@
 var web3 = new Web3(window.ethereum);
-
-var contract_address = "0xTODO";
-var user_address = window.ethereum.selectedAddress;
-
+if (typeof window.ethereum === "undefined") {
+    alert("Please install MetaMask") // AC-305-01
+} else {
 // Initial function
-async function init() {
-    // it is assumed that the metamask is installed, so we dont need to check this
-    await ethereum.enable();
-}
-init();
+    async function init() {
+        if (typeof window.ethereum === "undefined") {
+            alert("Please install MetaMask") // AC-305-01
+        } else {
+            // var abi;
+            var contract_address = "0xTODO";
+            var user_address = window.ethereum.selectedAddress;
+            fetch("static/js/abi/Sheremetyevo.json")
+                .then(response => {
+                    return response.json();
+                })
+                .then(
+                    abi => {
+                        var contract = new web3.eth.Contract(abi, contract_address);
+                        console.log(contract);
+                    }
+                );
+            // var contract = new web3.eth.Contract(abi, contract_address);
+            // console.log(user_address, contract, contract_address);
+        }
+    }
 
-function is_not_valid(address) {
-    return (address === "") || (!web3.utils.isAddress(address)) || (!address.startsWith('0x'));
-}  
+    init();
 
-async function buy() {
-}
+    function is_not_valid(address) {
+        return (address === "") || (!web3.utils.isAddress(address)) || (!address.startsWith('0x'));
+    }
 
-async function withdraw() {
+    async function buy() {
+    }
+
+    async function withdraw() {
+    }
 }
