976e25abb60a0a06a7897cd26bf2927a1b8386d6
Evgenij Ivankin <e.ivankin@innopolis.university>
2021-09-04 13:43:58 +0300

    add contract

---
+++ contract/contracts/Sheremetyevo.sol
@@ -0,0 +1,105 @@
+pragma solidity ^0.7.6;
+
+library IterableMapping {
+    // Iterable mapping from address to uint;
+    struct Map {
+        address[] keys;
+        mapping(address => uint256) values;
+        mapping(address => uint) indexOf;
+        mapping(address => bool) inserted;
+    }
+
+    function get(Map storage map, address key) public view returns (uint) {
+        return map.values[key];
+    }
+
+    function getKeyAt(Map storage map, uint index) public view returns (address) {
+        return map.keys[index];
+    }
+
+    function size(Map storage map) public view returns (uint) {
+        return map.keys.length;
+    }
+
+    function set(
+        Map storage map,
+        address key,
+        uint val
+    ) public {
+        if (map.inserted[key]) {
+            map.values[key] = val;
+        } else {
+            map.inserted[key] = true;
+            map.values[key] = val;
+            map.indexOf[key] = map.keys.length;
+            map.keys.push(key);
+        }
+    }
+
+    function remove(Map storage map, address key) public {
+        if (!map.inserted[key]) {
+            return;
+        }
+
+        delete map.inserted[key];
+        delete map.values[key];
+
+        uint index = map.indexOf[key];
+        uint lastIndex = map.keys.length - 1;
+        address lastKey = map.keys[lastIndex];
+
+        map.indexOf[lastKey] = index;
+        delete map.indexOf[key];
+
+        map.keys[index] = lastKey;
+        map.keys.pop();
+    }
+}
+
+contract Sheremetyevo {
+    using IterableMapping for IterableMapping.Map;
+
+    address private _owner;
+    mapping(address => uint256) private _tariffs;
+    IterableMapping.Map private _balances;
+
+    modifier only_for_owner() {
+        require(msg.sender == _owner, "Only owner can call this method");
+        _;
+    }
+
+    constructor() {
+        _owner = msg.sender;
+    }
+
+    function set(address user, uint256 tariff) public only_for_owner {
+        _tariffs[user] = tariff;
+    }
+
+    function setBalance(address user, uint256 balance) public only_for_owner {
+        _balances.set(user, balance);
+    }
+
+    function withdraw(address from, address to, uint256 amount) public only_for_owner {
+        require(_balances.get(from) >= amount, "Balance is not enough");
+    }
+
+    function getBalance(address user) external view returns (uint256) {
+        return _balances.get(user);
+    }
+
+    function getTariff(address user) external view returns (uint256) {
+        return _tariffs[user];
+    }
+
+    function iterDay() public only_for_owner {
+        for (uint i = 0; i < _balances.size(); i++) {
+            address key = _balances.getKeyAt(i);
+            _balances.set(key, _balances.get(key) - _tariffs[key]);
+        }
+    }
+
+    fallback() external payable {
+        _balances.set(msg.sender, _balances.get(msg.sender) + msg.value);
+    }
+}
