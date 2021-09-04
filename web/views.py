f32879b28a89cd7be5a3838079b564858d893a9c
kerniee <karapys.mor@gmail.com>
2021-09-04 18:02:45 +0300

    fix

--- web/views.py
+++ web/views.py
@@ -1,4 +1,5 @@
 from flask import render_template
+from flask_login import current_user
 
 from connect_to_blockchain import contract_container
 from web import app, login_manager
@@ -13,7 +14,20 @@ def load_user(user_id):
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    user = User.query.get(current_user.get_id())
+    error, balance, tariff, name, transactions, account = [None] * 6
+    if user is None:
+        error = 'Пользователь не найден'
+    elif not user.is_renter:
+        error = 'Пользователь не является арендатором'
+    else:
+        balance = contract_container.getBalance(user.account)
+        tariff = contract_container.getTariff(user.account)
+        name = user.username
+        account = user.account
+        transactions = history.filter(sender=account)
+    return render_template('index.html', balance=balance, tariff=tariff, name=name,
+                           transactions=enumerate(transactions))
 
 
 @app.route('/renter/<renter_id>')
