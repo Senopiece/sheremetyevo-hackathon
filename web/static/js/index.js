f3738afdea5c25ea897f2ae1c510446e1fe2dd2c
Nabuki <shichiromail@gmail.com>
2021-09-04 21:24:44 +0300

    add withdraw func

--- web/static/js/index.js
+++ web/static/js/index.js
@@ -1,5 +1,21 @@
 var web3 = new Web3(window.ethereum);
 
+var contract_address = "0xTODO";
+var user_address = window.ethereum.selectedAddress;
+
+// Initial function
+async function init() {
+    // it is assumed that the metamask is installed, so we dont need to check this
+    await ethereum.enable();
+}
+init();
+
+function is_not_valid(address) {
+    return (address === "") || (!web3.utils.isAddress(address)) || (!address.startsWith('0x'));
+}  
+
 async function buy() {
-    
+}
+
+async function withdraw() {
 }
