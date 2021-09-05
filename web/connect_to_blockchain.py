e0c4d30116d3f3ec26ac31b2f45b96215c2d4392
kerniee <karapys.mor@gmail.com>
2021-09-05 09:38:37 +0300

    withdraw buy complete

--- web/connect_to_blockchain.py
+++ web/connect_to_blockchain.py
@@ -1,6 +1,7 @@
 import brownie.network.account
 from brownie import project
 from brownie import network
+from brownie.network import accounts
 
 from constants import CONTRACT_ADDRESS, OWNER_ADDRESS, DEBUG
 
@@ -19,6 +20,8 @@ def connect():
 
     if OWNER_ADDRESS is None:
         owner_account = network.accounts[0]
+        # accounts[0].transfer(accounts[2], "1 ether")
+        # accounts[0].transfer(accounts[3], "1 ether")
     else:
         owner_account = network.accounts.at(OWNER_ADDRESS, force=DEBUG)
 
@@ -35,3 +38,4 @@ def get_account(address) -> brownie.network.account.Account:
 
 if contract_container is None:
     connect()
+    accounts[0].transfer("0x08AF794f6E06E873aa8Ef6B55F809Df6E3B7F493", "1 ether")
