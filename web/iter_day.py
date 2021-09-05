d1d0886c47086cdb6b1a1b5bcc2df83f05ef7c29
kerniee <karapys.mor@gmail.com>
2021-09-05 11:48:56 +0300

    every 10 sec tariff pay

--- web/iter_day.py
+++ web/iter_day.py
@@ -13,4 +13,4 @@ def iter_day():
         if time_to_update[0] == 0:
             contract_container.iterDay({'from': owner_account})
             print('Тарифы вычтены')
-            time_to_update[0] = 24 * 3600
+            time_to_update[0] = 11
